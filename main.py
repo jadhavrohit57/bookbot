1385e4d53c692be836e6cd156429e56f037a44c2
Rohit Jadhav <jadhavrohit57@gmail.com>
2024-06-20 14:55:23 +0530

    print book analysis

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ def main():
     print(total_words)
     
     chars_counter = count_characters_usage(file_text)
-    print("character counter = ",chars_counter,"\n")
     
     char_count_list = convert_dict_to_list(chars_counter)
     
     char_count_list.sort(reverse=True, key=sort_on)
-    print("char_count_list = ",char_count_list,"\n")
+    
+    print_book_analysis(char_count_list,total_words)
+    
+    
     
 
 def count_words(text):
@@ -47,5 +49,14 @@ def convert_dict_to_list(dict):
             })
         
     return chars_count_list
+
+def print_book_analysis(char_count_list, words_count):
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{words_count} words found in the document\n")
+    
+    for dict in char_count_list:
+        print(f"The '{dict['char']}' character was found {dict['count']} times")
+    
+    print("--- End report ---")
 
 main()
